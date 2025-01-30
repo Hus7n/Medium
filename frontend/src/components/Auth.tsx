f53e2baf2d78994e0cfc2164b9d5102355393fc0
Hussain <shaikhhussi786@gmail.com>
2025-01-30 23:52:35 +0530

    sign up and sign in page

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,68 @@
+import { SignupInput } from "@hus7n/medium-common";
+import { ChangeEvent, useState } from "react";
+import { Link } from "react-router-dom"
+
+export const Auth = ({type} : {type :"signup" |"signin"}) =>{
+    const [postInputs , setPostInputs] = useState<SignupInput>({
+        name : "",
+        username :"",
+        password : ""
+    })
+    return <div className=" h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div>
+            <div className="px-10">
+                <div className="text-3xl font-extrabold">
+                        Create an account
+                </div>
+                <div className="text-slate-500">
+                    {type === "signin" ? "Dont have an account ?" : "Already have an account ?"}
+                    <Link className="pl-2 underline" to={type === "signin" ? "/signup" : "/signin"}>
+                    {type === "signin" ? "Sign Up " : "Sign In"}
+                    </Link>
+                </div>
+            </div>
+            <div className="pt-4">
+            <LabelledInput label="Name" placeholder="Name..." onChange={(e) =>{
+                setPostInputs({
+                    //intead of putting 3 field this will get all 
+                    ...postInputs,
+                    name: e.target.value
+                })
+            }}/>
+            <LabelledInput label="Username" placeholder="...@gamail.com" onChange={(e) =>{
+                setPostInputs({
+                    //intead of putting 3 field this will get all 
+                    ...postInputs,
+                    name: e.target.value
+                })
+            }}/>
+            <LabelledInput label="Password" type={"password"} placeholder="Password" onChange={(e) =>{
+                setPostInputs({
+                    //intead of putting 3 field this will get all prevent overriding
+                    ...postInputs,
+                    name: e.target.value
+                })
+            }}/>
+            <button type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium  text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign Up" : "Sign In"}</button>
+            </div> 
+            </div>
+        </div>
+    </div>
+}
+
+interface LabelledInput{
+    label : string;
+    onChange : (e : ChangeEvent<HTMLInputElement>) => void;
+    placeholder : string;
+    type ? : string
+}
+
+function LabelledInput({label , placeholder , onChange} : LabelledInput){
+    return <div>
+        <div>
+            <label  className="block mb-2 text-sm pt-4 text-black font-semibold">{label}</label>
+            <input onChange={onChange} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+        </div>
+    </div>
+}
---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,18 @@
+export const Quote = () => {
+    return <div className="h-screen bg-slate-200 flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-3xl font-bold">
+                "The customer support I received was exceptional.The support team went above and beyond to address my concern"
+                </div>
+                <div className="max-w-md  text-xl font-semibold text-left mt-4">
+            hus7n
+        </div>
+        <div className="max-w-md  text-sm font-light text-slate-400">
+            CEO
+        </div>
+            </div>
+        </div>
+        
+    </div>
+}
